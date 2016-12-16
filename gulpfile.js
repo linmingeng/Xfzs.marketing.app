8e286e2e240508d302b0a5bb4a12ebff7ac0b29f
unknown <>
2016-12-16 11:19:05 +0800

    修复bug

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var ftp = require('vinyl-ftp')
 
 gulp.task('ftp', function () {
     var conn = ftp.create({
-        host: ' 118.178.192.111',
+        host: '118.178.192.111',
         port: 22,
         user: 'administrator',
         password: '1QAZxsw23edc',
