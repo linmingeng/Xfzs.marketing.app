69e71e2d3ee9904ce176cf459dc039dcf8bc1ae0
linmingeng <351812469@qq.com>
2017-07-24 19:42:00 +0800

    fix : 修补bug

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ var ftp = require('vinyl-ftp')
 
 gulp.task('ftp', function () {
     var conn = ftp.create({
-        host: 'vote.hxzcgf.cn',
-        port: 215,
-        user: 'vote.app',
-        password: '96e7921E965eL72c92a549dd5a330112',
+        host: '118.178.192.111',
+        port: 22,
+        user: 'administrator',
+        password: 'Hxzcgf111200***',
         parallel: 3,
         log: gutil.log
     })
